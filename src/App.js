adf78511482b6a49e98ea47fb50658fac687095c
mafia917@naver.com <mafia917@naver.com>
2022-11-28 04:12:41 +0900

    상세페이지 나오게

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import Main from "./pages/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MovieDetail from "./pages/MovieDetail";
-import Movies from "./pages/Movies";
+
+import MovieInfo from "./pages/MovieInfo";
+
 
 function App() {
   return (
-    <div className="movie">
-      <BrowserRouter>
-        <Routes>
-          
-          <Route path="/" element={<Movies />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/movieInfo/:id" element={<MovieInfo />} />
+        
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/MovieInfo.jsx
@@ -0,0 +1,63 @@
+import { useParams } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
+import MovieData from "../MovieData.json";
+import Movie from '../components/Movie';
+import Component from '../components/Star';
+import LikeButton from '../components/LikeButton';
+
+export default function MovieInfo() {
+  const { id } = useParams();
+  
+
+
+  return (<div>
+    <img src={MovieData[id - 1].poster_url} >    
+    </img>
+    <br></br><br></br>
+    
+    <Component /> 
+    <LikeButton />
+    {MovieData[id - 1].title}의 상세정보 페이지입니다  
+
+    가격 : {MovieData[id- 1 ].price} 원
+    <br></br>
+    
+    <br></br><br></br>
+    관람등급: {MovieData[id - 1].flim_rating}
+    <br></br>
+    배급사:   {MovieData[id - 1].distributor}
+    <br></br>
+    장르:   {MovieData[id - 1].genres}
+    <br></br>
+    개봉일:   {MovieData[id - 1].release_date}
+    <br></br>
+    러닝타임: {MovieData[id - 1].runtime}
+    <br></br>
+    평점 :    {MovieData[id - 1].rating}
+    <br></br>
+    감독 :    {MovieData[id - 1].director}
+    <br></br>
+    배우 :    {MovieData[id - 1].actors}
+    <br></br> 
+    수상작:     {MovieData[id - 1].awards}
+    <br></br><br></br>
+    줄거리 :  {MovieData[id - 1].summary}
+    <br></br><br></br>
+    
+
+    트레일러 영상 보기:
+    <br></br>
+    <iframe width="500" height="400" src={MovieData[id - 1].youtube_trailer_url} title={MovieData[id - 1].title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    {MovieData[id - 1].youtube_trailer_url}
+
+    
+
+
+
+      </div>);
+
+
+
+}
+
+
